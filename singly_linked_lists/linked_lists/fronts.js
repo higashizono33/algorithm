62d43071702e9050746b7b72ad20721d5758ce3d
= <takashizono@gmail.com>
2021-07-06 20:59:55 -0500

    made sll fronts.js assignment

---
+++ singly_linked_lists/linked_lists/fronts.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// Add Front
+// Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head nod
+class SLL {
+     // constructor, other methods, removed for brevity
+    constructor(){
+        this.head = null
+        this.length = 0
+    }
+    addFront(value){
+        newNode = new Node(value)
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+
+        return this.head
+    }
+
+    removeFront(){
+        if(this.head=null){
+            return null
+        }else{
+            while(this.next == null){
+                this = this.next
+            }
+        }
+        this.length--
+        return this.head
+    }
+
+    front(){
+        if(this.head=null){
+            return null
+        }else{
+            return this.head.value
+        }
+    }
+}
+
